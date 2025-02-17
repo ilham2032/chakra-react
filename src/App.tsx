4e74b112b969aa2265033212d8a3f312b53e9215
unknown <you@example.com>
2025-02-17 22:46:04 +0400

    .

---
+++ src/App.tsx
@@ -0,0 +1,391 @@
+import { Card } from "@chakra-ui/react"
+import { Avatar } from "./components/ui/avatar"
+
+const App = () => {
+  return (
+    <div>
+
+        <h1 className="text-white text-center font-semibold" >Rick and Morty Characters</h1>
+
+    <div className="flex max-w-[1800px]">
+      <Card.Root width="520px" margin={"30px"} >
+      <Card.Body gap="2">
+        <Avatar
+          boxSize={"200px"}
+          marginLeft={"134px"}
+          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+          name="Nue Camp"
+        />
+        <Card.Title mt="2" textAlign={"center"} fontSize={"25px"}>Rick Sanchez</Card.Title>
+        <Card.Description color={"white"} textAlign={"center"} fontSize={"17px"}>
+          Species: Human
+          <br />
+          Status: Alive
+        </Card.Description>
+      </Card.Body>
+    </Card.Root>
+
+
+    <Card.Root width="520px" margin={"30px"} >
+      <Card.Body gap="2">
+        <Avatar
+          boxSize={"200px"}
+          marginLeft={"134px"}
+          src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+          name="Nue Camp"
+        />
+        <Card.Title mt="2" textAlign={"center"} fontSize={"25px"}>Morty Smith</Card.Title>
+        <Card.Description color={"white"} textAlign={"center"} fontSize={"17px"}>
+          Species: Human
+          <br />
+          Status: Alive
+        </Card.Description>
+      </Card.Body>
+    </Card.Root>
+
+    <Card.Root width="520px" margin={"30px"} >
+      <Card.Body gap="2">
+        <Avatar
+          boxSize={"200px"}
+          marginLeft={"134px"}
+          src="https://rickandmortyapi.com/api/character/avatar/3.jpeg"
+          name="Nue Camp"
+        />
+        <Card.Title mt="2" textAlign={"center"} fontSize={"25px"}>Summer Smith</Card.Title>
+        <Card.Description color={"white"} textAlign={"center"} fontSize={"17px"}>
+          Species: Human
+          <br />
+          Status: Alive
+        </Card.Description>
+      </Card.Body>
+    </Card.Root>
+
+    </div>
+
+    <div className="flex max-w-[1800px]">
+
+    <Card.Root width="520px" margin={"30px"} >
+      <Card.Body gap="2">
+        <Avatar
+          boxSize={"200px"}
+          marginLeft={"134px"}
+          src="https://rickandmortyapi.com/api/character/avatar/4.jpeg"
+          name="Nue Camp"
+        />
+        <Card.Title mt="2" textAlign={"center"} fontSize={"25px"}>Beth Smith</Card.Title>
+        <Card.Description color={"white"} textAlign={"center"} fontSize={"17px"}>
+          Species: Human
+          <br />
+          Status: Alive
+        </Card.Description>
+      </Card.Body>
+    </Card.Root>
+
+    <Card.Root width="520px" margin={"30px"} >
+      <Card.Body gap="2">
+        <Avatar
+          boxSize={"200px"}
+          marginLeft={"134px"}
+          src="https://rickandmortyapi.com/api/character/avatar/5.jpeg"
+          name="Nue Camp"
+        />
+        <Card.Title mt="2" textAlign={"center"} fontSize={"25px"}>Jerry Smith</Card.Title>
+        <Card.Description color={"white"} textAlign={"center"} fontSize={"17px"}>
+          Species: Human
+          <br />
+          Status: Alive
+        </Card.Description>
+      </Card.Body>
+    </Card.Root>
+
+    <Card.Root width="520px" margin={"30px"} >
+      <Card.Body gap="2">
+        <Avatar
+          boxSize={"200px"}
+          marginLeft={"134px"}
+          src="https://rickandmortyapi.com/api/character/avatar/6.jpeg"
+          name="Nue Camp"
+        />
+        <Card.Title mt="2" textAlign={"center"} fontSize={"25px"}>Abadango Cluster Princess</Card.Title>
+        <Card.Description color={"white"} textAlign={"center"} fontSize={"17px"}>
+          Species: Alien
+          <br />
+          Status: Alien
+        </Card.Description>
+      </Card.Body>
+    </Card.Root>
+
+    </div>
+
+
+    <div className="flex max-w-[1800px]">
+
+    <Card.Root width="520px" margin={"30px"} >
+      <Card.Body gap="2">
+        <Avatar
+          boxSize={"200px"}
+          marginLeft={"134px"}
+          src="https://rickandmortyapi.com/api/character/avatar/7.jpeg"
+          name="Nue Camp"
+        />
+        <Card.Title mt="2" textAlign={"center"} fontSize={"25px"}>Abradolf Lincler</Card.Title>
+        <Card.Description color={"white"} textAlign={"center"} fontSize={"17px"}>
+          Species: Human
+          <br />
+          Status: unknown
+        </Card.Description>
+      </Card.Body>
+    </Card.Root>
+
+    <Card.Root width="520px" margin={"30px"} >
+      <Card.Body gap="2">
+        <Avatar
+          boxSize={"200px"}
+          marginLeft={"134px"}
+          src="https://rickandmortyapi.com/api/character/avatar/8.jpeg"
+          name="Nue Camp"
+        />
+        <Card.Title mt="2" textAlign={"center"} fontSize={"25px"}>Adjudicator Rick</Card.Title>
+        <Card.Description color={"white"} textAlign={"center"} fontSize={"17px"}>
+          Species: Human
+          <br />
+          Status: Dead
+        </Card.Description>
+      </Card.Body>
+    </Card.Root>
+
+    <Card.Root width="520px" margin={"30px"} >
+      <Card.Body gap="2">
+        <Avatar
+          boxSize={"200px"}
+          marginLeft={"134px"}
+          src="https://rickandmortyapi.com/api/character/avatar/9.jpeg"
+          name="Nue Camp"
+        />
+        <Card.Title mt="2" textAlign={"center"} fontSize={"25px"}>Agency Director</Card.Title>
+        <Card.Description color={"white"} textAlign={"center"} fontSize={"17px"}>
+          Species: Human
+          <br />
+          Status: Dead
+        </Card.Description>
+      </Card.Body>
+    </Card.Root>
+
+    </div>
+
+
+    <div className="flex max-w-[1800px]">
+
+    <Card.Root width="520px" margin={"30px"} >
+      <Card.Body gap="2">
+        <Avatar
+          boxSize={"200px"}
+          marginLeft={"134px"}
+          src="https://rickandmortyapi.com/api/character/avatar/10.jpeg"
+          name="Nue Camp"
+        />
+        <Card.Title mt="2" textAlign={"center"} fontSize={"25px"}>Alan Rails</Card.Title>
+        <Card.Description color={"white"} textAlign={"center"} fontSize={"17px"}>
+          Species: Human
+          <br />
+          Status: Dead
+        </Card.Description>
+      </Card.Body>
+    </Card.Root>
+
+    <Card.Root width="520px" margin={"30px"} >
+      <Card.Body gap="2">
+        <Avatar
+          boxSize={"200px"}
+          marginLeft={"134px"}
+          src="https://rickandmortyapi.com/api/character/avatar/11.jpeg"
+          name="Nue Camp"
+        />
+        <Card.Title mt="2" textAlign={"center"} fontSize={"25px"}>Albert Einstein</Card.Title>
+        <Card.Description color={"white"} textAlign={"center"} fontSize={"17px"}>
+          Species: Human
+          <br />
+          Status: Dead
+        </Card.Description>
+      </Card.Body>
+    </Card.Root>
+
+    <Card.Root width="520px" margin={"30px"} >
+      <Card.Body gap="2">
+        <Avatar
+          boxSize={"200px"}
+          marginLeft={"134px"}
+          src="https://rickandmortyapi.com/api/character/avatar/12.jpeg"
+          name="Nue Camp"
+        />
+        <Card.Title mt="2" textAlign={"center"} fontSize={"25px"}>Alexander</Card.Title>
+        <Card.Description color={"white"} textAlign={"center"} fontSize={"17px"}>
+          Species: Human
+          <br />
+          Status: Dead
+        </Card.Description>
+      </Card.Body>
+    </Card.Root>
+
+    </div>
+
+
+    <div className="flex max-w-[1800px]">
+
+    <Card.Root width="520px" margin={"30px"} >
+      <Card.Body gap="2">
+        <Avatar
+          boxSize={"200px"}
+          marginLeft={"134px"}
+          src="https://rickandmortyapi.com/api/character/avatar/13.jpeg"
+          name="Nue Camp"
+        />
+        <Card.Title mt="2" textAlign={"center"} fontSize={"25px"}>Alien Googah</Card.Title>
+        <Card.Description color={"white"} textAlign={"center"} fontSize={"17px"}>
+          Species: Alien
+          <br />
+          Status: unknown
+        </Card.Description>
+      </Card.Body>
+    </Card.Root>
+
+    <Card.Root width="520px" margin={"30px"} >
+      <Card.Body gap="2">
+        <Avatar
+          boxSize={"200px"}
+          marginLeft={"134px"}
+          src="https://rickandmortyapi.com/api/character/avatar/14.jpeg"
+          name="Nue Camp"
+        />
+        <Card.Title mt="2" textAlign={"center"} fontSize={"25px"}>Alien Morty</Card.Title>
+        <Card.Description color={"white"} textAlign={"center"} fontSize={"17px"}>
+          Species: Alien
+          <br />
+          Status: unknown
+        </Card.Description>
+      </Card.Body>
+    </Card.Root>
+
+    <Card.Root width="520px" margin={"30px"} >
+      <Card.Body gap="2">
+        <Avatar
+          boxSize={"200px"}
+          marginLeft={"134px"}
+          src="https://rickandmortyapi.com/api/character/avatar/15.jpeg"
+          name="Nue Camp"
+        />
+        <Card.Title mt="2" textAlign={"center"} fontSize={"25px"}>Alien Rick</Card.Title>
+        <Card.Description color={"white"} textAlign={"center"} fontSize={"17px"}>
+          Species: Alien
+          <br />
+          Status: unknown
+        </Card.Description>
+      </Card.Body>
+    </Card.Root>
+
+    </div>
+
+
+    <div className="flex max-w-[1800px]">
+
+    <Card.Root width="520px" margin={"30px"} >
+      <Card.Body gap="2">
+        <Avatar
+          boxSize={"200px"}
+          marginLeft={"134px"}
+          src="https://rickandmortyapi.com/api/character/avatar/16.jpeg"
+          name="Nue Camp"
+        />
+        <Card.Title mt="2" textAlign={"center"} fontSize={"25px"}>Amish Cyborg</Card.Title>
+        <Card.Description color={"white"} textAlign={"center"} fontSize={"17px"}>
+          Species: Alien
+          <br />
+          Status: Dead
+        </Card.Description>
+      </Card.Body>
+    </Card.Root>
+
+    <Card.Root width="520px" margin={"30px"} >
+      <Card.Body gap="2">
+        <Avatar
+          boxSize={"200px"}
+          marginLeft={"134px"}
+          src="https://rickandmortyapi.com/api/character/avatar/17.jpeg"
+          name="Nue Camp"
+        />
+        <Card.Title mt="2" textAlign={"center"} fontSize={"25px"}>Annie</Card.Title>
+        <Card.Description color={"white"} textAlign={"center"} fontSize={"17px"}>
+          Species: Human
+          <br />
+          Status: Alive
+        </Card.Description>
+      </Card.Body>
+    </Card.Root>
+
+    <Card.Root width="520px" margin={"30px"} >
+      <Card.Body gap="2">
+        <Avatar
+          boxSize={"200px"}
+          marginLeft={"134px"}
+          src="https://rickandmortyapi.com/api/character/avatar/18.jpeg"
+          name="Nue Camp"
+        />
+        <Card.Title mt="2" textAlign={"center"} fontSize={"25px"}>Antenna Morty</Card.Title>
+        <Card.Description color={"white"} textAlign={"center"} fontSize={"17px"}>
+          Species: Human
+          <br />
+          Status: Alive
+        </Card.Description>
+      </Card.Body>
+    </Card.Root>
+
+    </div>
+
+
+    <div className="flex max-w-[1800px]">
+
+    <Card.Root width="520px" margin={"30px"} >
+      <Card.Body gap="2">
+        <Avatar
+          boxSize={"200px"}
+          marginLeft={"134px"}
+          src="https://rickandmortyapi.com/api/character/avatar/19.jpeg"
+          name="Nue Camp"
+        />
+        <Card.Title mt="2" textAlign={"center"} fontSize={"25px"}>Antenna Rick</Card.Title>
+        <Card.Description color={"white"} textAlign={"center"} fontSize={"17px"}>
+          Species: Human
+          <br />
+          Status: unknown
+        </Card.Description>
+      </Card.Body>
+    </Card.Root>
+
+    <Card.Root width="520px" margin={"30px"} >
+      <Card.Body gap="2">
+        <Avatar
+          boxSize={"200px"}
+          marginLeft={"134px"}
+          src="https://rickandmortyapi.com/api/character/avatar/20.jpeg"
+          name="Nue Camp"
+        />
+        <Card.Title mt="2" textAlign={"center"} fontSize={"25px"}>Ants in my Eyes Johnson</Card.Title>
+        <Card.Description color={"white"} textAlign={"center"} fontSize={"17px"}>
+          Species: Human
+          <br />
+          Status: unknown
+        </Card.Description>
+      </Card.Body>
+    </Card.Root>
+
+    </div>
+
+    
+
+    </div>
+
+
+  )
+}
+
+export default App
